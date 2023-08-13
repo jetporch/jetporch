db32ed25a0e98120f1c1ffedb3e0dd4fc74f05e4
Michael DeHaan <michael@michaeldehaan.net>
2023-08-13 10:18:01 -0400

    missing file

---
+++ src/connection/command.rs
@@ -0,0 +1,29 @@
+// Jetporch
+// Copyright (C) 2023 - Michael DeHaan <michael@michaeldehaan.net> + contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// long with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+pub struct Command {
+    cmd: String,
+}
+
+impl Command {
+
+    pub fn new(cmd: message) -> Self {
+        Self { 
+            cmd: cmd.clone(),
+        }
+    }
+
+}
